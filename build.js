3c72b7168344df59836675ae995a2da3180d29ef
Ernesto De Los Santos <netos_id@yahoo.com>
2016-10-06 11:40:59 -0500

    Removed the form and inputs from the index and had it render through different file.

--- build.js
+++ build.js
@@ -1,13 +1,9 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { Router, Route, hashHistory } from 'react-router'
 import Home from './lib/Home'
-import { Link } from 'react-router'
-import TakeClass from './lib/TakeClass'
+// import TakeClass from './lib/TakeClass'
 
 render((
-  <Router history={ hashHistory }>
-    <Route path="/" component={ Home }/>
-    <Route path="/TakeClass" component={ TakeClass }/>
-  </Router>
-),document.getElementById('app'))
+  <Home name="Home"/>
+
+), document.getElementById('app'));
